e870cf42bcdbbf3dcecdbff463ec854746dfb40e
Kabir Baidhya <kabirbaidhya@gmail.com>
2020-04-16 20:13:56 +0545

    Update release script

--- packages/db-model/release.sh
+++ packages/db-model/release.sh
@@ -7,19 +7,20 @@ set -e
 changelog() {
   # NOTE: This requires github_changelog_generator to be installed.
   # https://github.com/skywinder/github-changelog-generator
+  TAG_NAME=$1
 
-  if [ -z "$NEXT" ]; then
-      NEXT="Unreleased"
+  if [ -z "$TAG_NAME" ]; then
+    TAG_NAME="Unreleased"
   fi
 
-  echo "Generating changelog upto version: $NEXT"
+  echo "Generating changelog upto version: $TAG_NAME"
   github_changelog_generator \
     --no-verbose \
     --pr-label "**Changes**" \
     --bugs-label "**Bug Fixes**" \
     --issues-label "**Closed Issues**" \
     --issue-line-labels=ALL \
-    --future-release="$NEXT" \
+    --future-release="$TAG_NAME" \
     --release-branch=master \
     --exclude-labels=unnecessary,duplicate,question,invalid,wontfix
 }
@@ -36,7 +37,7 @@ bump() {
   sed -i "s/\(\"version\":\s*\"\)[^\"]*\(\"\)/\1${VERSION}\2/g" package.json
 
   # Generate change log
-  changelog
+  changelog $TAG_NAME
   echo ""
 
   # Generate new build.
@@ -44,11 +45,11 @@ bump() {
 
   # Prepare to commit
   git add CHANGELOG.md package.json yarn.lock && \
-    git commit -v --edit -m "Release ${TAG_NAME}"
-  #   git tag "${TAG_NAME}" && \
-  #   echo -e "\nRelease tagged $TAG_NAME"
-  # git push origin HEAD --tags
-  # yarn publish --new-version "${VERSION}" --no-git-tag-version
+    git commit -v --edit -m "Release ${TAG_NAME}" && \
+    git tag "${TAG_NAME}" && \
+    echo -e "\nRelease tagged $TAG_NAME"
+  git push origin HEAD --tags
+  yarn publish --new-version "${VERSION}" --no-git-tag-version
 }
 
 # Run command received from args.
